f09f0a87729373d17568b403a4170d5732d95ab6
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-07-12 11:13:00 +0530

    Add Generate Id Method

--- student management system/src/controller/StudentFormController.java
+++ student management system/src/controller/StudentFormController.java
@@ -2,9 +2,19 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import view.tdm.StudentTM;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class StudentFormController {
     public AnchorPane StudentManageFormContext;
@@ -19,6 +29,9 @@ public class StudentFormController {
     public JFXButton btnDelete;
     public TableView tblStudent;
 
+    public void initialize(){
+
+    }
 
     public void btnAddNewStudentOnAction(ActionEvent actionEvent) {
     }
@@ -30,4 +43,37 @@ public class StudentFormController {
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
     }
+    private String generateNewId() {
+        try {
+            Connection connection = DBConnection.getDbConnection().getConnection();
+            ResultSet rst = connection.createStatement().executeQuery("SELECT studentId FROM Student ORDER BY studentId DESC LIMIT 1;");
+            if (rst.next()) {
+                String id = rst.getString("id");
+                int newCustomerId = Integer.parseInt(id.replace("S-", "")) + 1;
+                return String.format("S-%03d", newCustomerId);
+            } else {
+                return "C00-001";
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "Failed to generate a new id " + e.getMessage()).show();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        if (tblStudent.getItems().isEmpty()) {
+            return "C00-001";
+        } else {
+            String id = getLastCustomerId();
+            int newCustomerId = Integer.parseInt(id.replace("C", "")) + 1;
+            return String.format("C00-%03d", newCustomerId);
+        }
+
+    }
+    private String getLastCustomerId() {
+        List<StudentTM> tempStudentsList = new ArrayList<>(tblStudent.getItems());
+        Collections.sort(tempStudentsList);
+        return tempStudentsList.get(tempStudentsList.size() - 1).getId();
+    }
+
 }
