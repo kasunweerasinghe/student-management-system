2955b0370c664335e42c646b142f800ff032105d
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-07-12 10:54:51 +0530

    Added UI and Refactor

--- student management system/src/controller/StudentFormController.java
+++ student management system/src/controller/StudentFormController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentFormController {
+    public AnchorPane StudentManageFormContext;
+    public JFXTextField txtStudentID;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNIc;
+    public JFXButton btnAddNewStudent;
+    public JFXButton btnSave;
+    public JFXButton btnDelete;
+    public TableView tblStudent;
+
+
+    public void btnAddNewStudentOnAction(ActionEvent actionEvent) {
+    }
+
+   
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
 }
