66f1cc7abd2bece58cdc8c5989619edc8902fae4
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-07-12 11:06:28 +0530

    Add StudentTM

--- student management system/src/view/tdm/StudentTM.java
+++ student management system/src/view/tdm/StudentTM.java
@@ -1,4 +1,70 @@
 package view.tdm;
 
 public class StudentTM {
+    private String id;
+    private String name;
+    private String email;
+    private String contact;
+    private String address;
+    private String nic;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentTM{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", contact='" + contact + '\'' +
+                ", address='" + address + '\'' +
+                ", nic='" + nic + '\'' +
+                '}';
+    }
 }
